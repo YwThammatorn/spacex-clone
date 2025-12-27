7eb367e6bb4cf0f333d6dcb2ea9380ba61cc6ba1
YwThammatron <thammatron135@gmail.com>
2025-12-28 02:35:31 +0700

    add reponsive to navbar and footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,12 +2,12 @@ import { Twitter } from "lucide-react"
 
 const Footer = () => {
   return (
-    <div className='flex justify-between items-center gap-[46px] w-full h-[4.625rem] pl-[60px] pr-[60px] bg-[linear-gradient(#00000000_95%,#00000099_0%)]'>
+    <div className='flex justify-between items-center gap-[46px] w-full h-[4.625rem] pl-[60px] pr-[60px] bg-[linear-gradient(#00000000_95%,#00000099_0%)] max-phone:flex-col'>
       {/* button */}
       <a target='_blank' className="p-[18px] rounded-[2rem] bg-[var(--white-10)] hover:cursor-pointer"><Twitter width={12} height={12} /></a>
       
       {/* links */}
-      <div className="flex gap-[2.3125rem] text-[0.813rem] uppercase font-ddin font-light">
+      <div className="flex gap-[2.3125rem] text-[0.813rem] uppercase font-ddin font-light max-phone:text-[0.75rem] gap-[12px]">
         <p className="hover:cursor-pointer hover:text-[var(--hover-color)]">Careers</p>
         <p className="hover:cursor-pointer hover:text-[var(--hover-color)]">Updates</p>
         <p className="hover:cursor-pointer hover:text-[var(--hover-color)]">Privacy policy</p>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image"
 import Link from "next/link"
 
+import { Menu } from "lucide-react"
+
 const Navbar = () => {
   return (
-    <div className='fixed z-10 flex justify-start items-center gap-[46px] w-full h-[4.625rem] pl-[60px] pr-[60px] bg-[linear-gradient(#00000099_0%,#00000000_95%)]'>
+    <div className='fixed z-10 flex justify-start items-center gap-[46px] w-full h-[4.625rem] pl-[3.945%] pr-[3.945%] bg-[linear-gradient(#00000099_0%,#00000000_95%)] max-ipad:justify-between'>
       {/* logo */}
       <div>
         <Image src="/spacex_logo.png" alt="spacex_logo"
@@ -13,7 +15,7 @@ const Navbar = () => {
       </div>
 
       {/* links */}
-      <div className="flex gap-[2.3125rem] text-[0.813rem] tracking-[0.09em] uppercase font-ddin font-[700]">
+      <div className="flex gap-[2.3125rem] text-[0.813rem] tracking-[0.09em] uppercase font-ddin font-[700] max-ipad:hidden">
         <Link href={''} className="hover:cursor-pointer hover:text-[var(--white-hover)]">Vehicles</Link>
         <Link href={''} className="hover:cursor-pointer hover:text-[var(--white-hover)]">Launches</Link>
         <Link href={''} className="hover:cursor-pointer hover:text-[var(--white-hover)]">Human spaceflight</Link>
@@ -23,6 +25,9 @@ const Navbar = () => {
         <Link href={''} className="hover:cursor-pointer hover:text-[var(--white-hover)]">Company</Link>
         <Link href={''} className="hover:cursor-pointer hover:text-[var(--white-hover)]">Shop</Link>
       </div>
+
+      {/* ipad button */}
+      <button className="bg-transparent border-none max-pc:hidden max-ipad:flex"><Menu width={24} height={24} /></button>
     </div>
   )
 }
